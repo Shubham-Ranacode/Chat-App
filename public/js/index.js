1bbba5d52665a481c6872b79bc3c76c98a7955b0
Shubham Rana <shubhamrana196@gmail.cpm>
2020-06-20 15:49:15 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,7 @@ var socket=io();
 socket.on('connect',function(){
     console.log('connected to server');
 
-    socket.emit('createMessage',{
-        from:'andrew@example.com',
-        text:'Yup!thats work for me'
-    });
+    
 });
 
 socket.on('disconnect',function(){
